3ba5c3b24785b5a05d411f0dd222d4de2edccb49
Gavin Wang <ooboqoo@gmail.com>
2025-11-03 15:42:08 +0800

    Refactor: Delete redundant type definitions

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -101,6 +101,8 @@ const api = {
   }
 }
 
+export type MainAPI = typeof api
+
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
